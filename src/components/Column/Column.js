b26bd86211380ff36f41ad54209fee5f78b9eda3
kacpibanys <kacpibanys@gmail.com>
2021-10-01 11:53:19 +0200

    column title problem

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 
 class Column extends React.Component {
     static propTypes = {
-        columnTitle: PropTypes.string.isRequired,
+        columnProps: PropTypes.string.isRequired,
     }
 
     render(){
         return(
             <section className={styles.component}>
-                <h3 className={styles.title}>{this.props.columnTitle}</h3>
+                <h3 className={styles.title}>{this.props.columnProps}</h3>
             </section>
         )
     }
