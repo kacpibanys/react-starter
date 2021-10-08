f54228dfa8d489c42f20564dd16c44fa795e26d3
kacpibanys <kacpibanys@gmail.com>
2021-10-08 16:55:01 +0200

    add column

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import Column from '../Column/ColumnContainer';
 import {settings} from '../../data/dataStore.js';
 import ReactHtmlParser from 'react-html-parser';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
 
@@ -14,6 +14,7 @@ class List extends React.Component {
     description: PropTypes.node,
     columns: PropTypes.array,
     image: PropTypes.string,
+    addColumn: PropTypes.func,
   }
 
   static defaultProps = {
@@ -22,7 +23,7 @@ class List extends React.Component {
 
 
   render() {
-    const {title, image, description, columns} = this.props;
+    const {title, image, description, columns, addColumn} = this.props;
     return (
 
       <section className={styles.component}>
@@ -35,9 +36,9 @@ class List extends React.Component {
             <Column key={columnData.id} {...columnData} />
           ))}
         </div>
-        {/* <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
-        </div> */}
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={addColumn}/>
+        </div>
       </section>
     );
   }
